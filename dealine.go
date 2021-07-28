4944d2cf4fa248a75413537dfc8d0054c972155a
support <support@galaxynetwork.vip>
2021-07-28 16:55:24 +0800

    v0.3.1

--- dealine.go
+++ dealine.go
@@ -7,8 +7,9 @@ import (
 
 func CalculateDeadline(pid [32]byte,challenge []byte,difficulty *big.Int) *big.Int {
 	final := sha256.Sum256(append(pid[:],challenge...))
-	f := []byte{final[3],final[2],final[1],final[0]}
+	f := []byte{final[7],final[6],final[5],final[4],final[3],final[2],final[1],final[0]}
 	fin := big.NewInt(0).SetBytes(f)
+	fin.Div(fin,big.NewInt(1000))
 	fb := big.NewInt(0).Div(difficulty,fin)
 	return fb
 }
--- generator_test.go
+++ generator_test.go
@@ -1,17 +1,26 @@
 package poc
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"math/big"
+	"math/rand"
 	"testing"
 )
 
 func TestGenerator(t *testing.T) {
 
-	ge := GetGenerator()
-	if ge == nil {
-		fmt.Println("-------------------")
-	}else {
-		fmt.Println("*********************")
+	diff := big.NewInt(0).SetUint64(130310957598748952)
+	for i:=0;i<100;i++  {
+		pid := sha256.Sum256([]byte(fmt.Sprintf("sdaf%d",rand.Intn(1000000000))))
+		ch := sha256.Sum256([]byte(fmt.Sprintf("sdaf%d",rand.Intn(1000000000))))
+		d := CalculateDeadline(pid,ch[:],diff)
+		if d.Cmp(big.NewInt(18)) < 0 {
+			aj := big.NewInt(0).Div(diff,big.NewInt(18))
+			diff.Add(diff,aj)
+			fmt.Println("diff--------->",diff)
+		}
+		fmt.Println("deadline------>",d)
 	}
-
+	//115792089237316195423570985008687907853269984665640564039457584007913129639936
 }
